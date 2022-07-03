365a856a5354811583b7dc605795fcc6d65660b9
Garik <67053915+lGarikl@users.noreply.github.com>
2022-07-03 23:49:20 +0400

    Add files via upload

--- Assets/flyzeliafly.cs
+++ Assets/flyzeliafly.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class flyzeliafly : MonoBehaviour
 {
     public float velocity = 1;
-    
+    public Text gameText;
+    private int gameScore;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        int gameScore = 0;
+        gameText.text = "" + gameScore;
     }
 
     // Update is called once per frame
@@ -21,12 +25,13 @@ public class flyzeliafly : MonoBehaviour
         
         var pos = Input.mousePosition;
         pos.z = transform.position.z;
-        pos = Camera.main.ScreenToWorldPoint(pos);
-        pos = transform.InverseTransformPoint(pos);
-        
+        pos = Camera.main.ScreenToWorldPoint(pos); 
+        pos = transform.InverseTransformPoint(pos); 
+        gameScore++;
+        gameText.text = "" + gameScore;
         Vector2 vec;
-        
-        vec = new Vector2(-pos[0], 1);
+        // Я зубрю ;)
+        vec = new Vector2(-pos[0], 1); //Гений, даже ВС код выделил
         
         rb.velocity = vec * velocity;
     }
